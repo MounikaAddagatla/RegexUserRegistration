b967c1d87f5eb3f1b701bb275db0e45b8b738e27
MounikaAddagatla <89588852+MounikaAddagatla@users.noreply.github.com>
2021-10-08 22:01:52 +0530

    [Mounika] Add: User FirstName Validation

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RegexUserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            // first name regex Validation//
+            UserDetails firstName = new UserDetails();
+            firstName.IsValidFirstName("Mounika");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegexUserRegistration/UserDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUserRegistration
+{
+    class UserDetails
+    {
+        const string FIRST_NAME = "^[A-Z]{1}[a-z]{3,}$";
+        public void IsValidFirstName(string firstname)
+        {
+            if (Regex.IsMatch(firstname, FIRST_NAME))
+            {
+                Console.WriteLine("FirstName is validated");
+                return;
+            }
+            Console.WriteLine("FirstName is not validated");
+        }
+    }
+}
